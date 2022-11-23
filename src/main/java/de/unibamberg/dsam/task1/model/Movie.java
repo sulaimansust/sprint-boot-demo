679aa5ace2b5e0084518bcd9c71f3b38df674c3a
Sulayman Md <sulayman.md@stud.uni-bamberg.de>
2022-11-23 15:51:32 +0100

    Add empty movie , constructor

--- src/main/java/de/unibamberg/dsam/task1/model/Movie.java
+++ src/main/java/de/unibamberg/dsam/task1/model/Movie.java
@@ -2,10 +2,12 @@ package de.unibamberg.dsam.task1.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /// Generate getter and setters.
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     private String id;
     private String title;
